37ff96ada3ed7fddf9a4128293e76b1c8fd4d5de
+ <+>
2023-04-18 17:06:50 -0400

    add to cart done

--- product/views.py
+++ product/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views import View
 from django.http import HttpResponse
 from django.contrib import messages
 from . import models
+
+from pprint import pprint
 
 
 class ProductList(ListView):
@@ -23,17 +25,43 @@ class ProductDetail(DetailView):
 
 class AddToCart(View):
     def get(self, *args, **kwargs):
-        http_referer = self.request.META['HTTP_REFERER']
+        http_referer = self.request.META.get(
+            'HTTP_REFERER',
+            reverse('product:list')
+        )
         variation_id = self.request.GET.get('vid')
 
         if not variation_id:
             messages.error(
                 self.request,
-                "Product doesn't exist"
+                'Produto não existe'
             )
             return redirect(http_referer)
 
         variation = get_object_or_404(models.Variation, id=variation_id)
+        variation_inventory = variation.inventory
+        product = variation.product
+
+        product_id = product.id
+        product_name = product.name
+        variation_name = variation.name or ''
+        unity_price = variation.price
+        unity_sale_price = variation.sale_price
+        quantity = 1
+        slug = product.slug
+        image = product.image
+
+        if image:
+            image = image.name
+        else:
+            image = ''
+
+        if variation.inventory < 1:
+            messages.error(
+                self.request,
+                'Item Out of Stock'
+            )
+            return redirect(http_referer)
 
         if not self.request.session.get('cart'):
             self.request.session['cart'] = {}
@@ -42,22 +70,86 @@ class AddToCart(View):
         cart = self.request.session['cart']
 
         if variation_id in cart:
-            # TODO: var exists in cart
-            pass
+            quantity_cart = cart[variation_id]['quantity']
+            quantity_cart += 1
+
+            if variation_inventory < quantity_cart:
+                messages.warning(
+                    self.request,
+                    f'Not enough items on stock {quantity_cart}x for '
+                    f'product "{product_name}". {variation_inventory}x '
+                    f'Added to cart.'
+                )
+                quantidade_carrinho = variation_inventory
+
+            cart[variation_id]['quantity'] = quantity_cart
+            cart[variation_id]['quantity_price'] = unity_price * \
+                quantity_cart
+            cart[variation_id]['quantity_sale_price'] = unity_sale_price * \
+                quantity_cart
         else:
-            # TODO; var doesnt exist in cart
-            pass
-        return HttpResponse(f'{variation.product} {variation.name}')
+            cart[variation_id] = {
+                'product_id': product_id,
+                'product_name': product_name,
+                'variation_name': variation_name,
+                'variation_id': variation_id,
+                'unity_price': unity_price,
+                'unity_sale_price': unity_sale_price,
+                'quantity_price': unity_price,
+                'quantity_sale_price': unity_sale_price,
+                'quantity': 1,
+                'slug': slug,
+                'image': image,
+            }
+
+        self.request.session.save()
+
+        messages.success(
+            self.request,
+            f'Product {product_name} {variation_name} added to your '
+            f'cart {cart[variation_id]["quantity"]}x.'
+        )
+
+        return redirect(http_referer)
 
 
 class RemoveFromCart(View):
     def get(self, *args, **kwargs):
-        return HttpResponse('RemoveFromCart')
+        http_referer = self.request.META.get(
+            'HTTP_REFERER',
+            reverse('product:list')
+        )
+        variation_id = self.request.GET.get('vid')
+
+        if not variation_id:
+            return redirect(http_referer)
+
+        if not self.request.session.get('cart'):
+            return redirect(http_referer)
+
+        if variation_id not in self.request.session['cart']:
+            return redirect(http_referer)
+
+        cart = self.request.session['cart'][variation_id]
+
+        messages.success(
+            self.request,
+            f'Item {cart["product_name"]} {cart["variation_name"]} '
+            f'removed from cart.'
+        )
+
+        del self.request.session['cart'][variation_id]
+        self.request.session.save()
+        return redirect(http_referer)
 
 
 class Cart(View):
     def get(self, *args, **kwargs):
-        return HttpResponse('Cart')
+        context = {
+            'cart': self.request.session.get('cart', {})
+        }
+
+        return render(self.request, 'product/cart.html', context)
 
 
 class Finalize(View):
--- salessquare/settings.py
+++ salessquare/settings.py
@@ -159,3 +159,7 @@ SESSION_SAVE_EVERY_REQUEST = False
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
