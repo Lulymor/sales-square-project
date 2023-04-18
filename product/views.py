2f6e07d63d20da0ea7f5e5c47fa43a9590ab8cba
+ <+>
2023-04-18 11:22:03 -0400

    adding to cart

--- product/views.py
+++ product/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views import View
 from django.http import HttpResponse
+from django.contrib import messages
 from . import models
 
 
@@ -22,7 +23,31 @@ class ProductDetail(DetailView):
 
 class AddToCart(View):
     def get(self, *args, **kwargs):
-        return HttpResponse('AddToCart')
+        http_referer = self.request.META['HTTP_REFERER']
+        variation_id = self.request.GET.get('vid')
+
+        if not variation_id:
+            messages.error(
+                self.request,
+                "Product doesn't exist"
+            )
+            return redirect(http_referer)
+
+        variation = get_object_or_404(models.Variation, id=variation_id)
+
+        if not self.request.session.get('cart'):
+            self.request.session['cart'] = {}
+            self.request.session.save()
+
+        cart = self.request.session['cart']
+
+        if variation_id in cart:
+            # TODO: var exists in cart
+            pass
+        else:
+            # TODO; var doesnt exist in cart
+            pass
+        return HttpResponse(f'{variation.product} {variation.name}')
 
 
 class RemoveFromCart(View):
